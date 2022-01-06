6e3a1d370710fa90858d7cba433986ecd27ca506
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-01-06 21:24:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pytgcalls import idle
-from driver.MusicPlayer import call_py, bot
+from driver.musicplayer import call_py, bot
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
