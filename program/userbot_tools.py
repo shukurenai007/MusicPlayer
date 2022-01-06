9f1a6cdb43596e0f33a204f13a5b77c0ad2a2222
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-01-06 21:32:01 +0530

    Update userbot_tools.py

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -1,5 +1,5 @@
 import asyncio
-from driver.MusicPlayer import user
+from driver.musicplayer import user
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
